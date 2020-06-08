f6fdb374a704ad816514aad68a9af18a349a8c2f
ShiHangwei <shihangwei@gmail.com>
2020-06-08 22:41:57 +0800

    init sln

---
+++ src/gis/SpatialModel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpatialModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
