0f823138d9b76169af75b9faf820764bf12ad6fc
ShiHangwei <shihangwei@gmail.com>
2020-06-09 00:44:54 +0800

    load image

---
+++ src/gis/SpatialModel/GridCompression/Method1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace SpatialModel.GridCompression
+{
+    class Method1: ICompress
+    {
+
+        public void DoCompress(Color[,] colors)
+        {
+
+
+            
+        }
+    }
+}
--- src/gis/SpatialModel/Program.cs
+++ src/gis/SpatialModel/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SpatialModel.GridCompression;
+using System;
+using System.Threading.Tasks.Sources;
+using System.Drawing;
 
 namespace SpatialModel
 {
@@ -7,6 +10,22 @@ namespace SpatialModel
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + @"Files/jugg.png";
+            using Image image = Image.FromFile(fileName);
+
+            Console.WriteLine($"file width: {image.Width}, height: {image.Height} .");
+
+            Color[,] colors = new Color[image.Width, image.Height];
+            var bitmap = image.Clone() as Bitmap;
+
+            for (int y = 0; y < bitmap.Height; y++)
+                for (int x = 0; x < bitmap.Width; x++)
+                    colors[x, y] = bitmap.GetPixel(x, y);
+
+            Method1 m1 = new Method1();
+            m1.DoCompress(colors);
+
+            Console.ReadKey();
         }
     }
 }
